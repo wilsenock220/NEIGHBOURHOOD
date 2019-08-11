ece2bf7a3a7c96bc8e2226f2075bdc3fc2a3f952
wilsenock220 <wilsenock220@gmail.com>
2019-08-11 09:53:10 +0300

    add Profile update form

--- neighbours/forms.py
+++ neighbours/forms.py
@@ -1,2 +1,10 @@
 from .models import Profile, Neighbour, Businesses, Feeds
 from django import forms
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+
+        model = Profile
+        exclude = ["user"]
+        widgets = {"neigbor": forms.Select()}
