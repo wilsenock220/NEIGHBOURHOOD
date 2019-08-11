dc8e33c3a36c472ef290754a77a923973c88efac
wilsenock220 <wilsenock220@gmail.com>
2019-08-11 09:47:11 +0300

    add index view

--- neighbours/views.py
+++ neighbours/views.py
@@ -4,3 +4,8 @@ from .models import Profile, Businesses, Neighbour, Feeds
 from django.http import HttpResponse, Http404
 from django.contrib.auth.decorators import login_required
 
+
+def index(request):
+    profile = Profile.objects.filter(user_id=request.user.id)
+    title = "Home"
+    return render(request, "index.html", {"profile": profile, "title": title})
