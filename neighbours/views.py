e5e389fccef20060c5111a1f9326119a3d5401c6
wilsenock220 <wilsenock220@gmail.com>
2019-08-11 09:46:32 +0300

    import forms and models in views

--- neighbours/views.py
+++ neighbours/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ProfileForm, BusinessForm, PostForm, UpdateForm, ChangeHood
+from .models import Profile, Businesses, Neighbour, Feeds
+from django.http import HttpResponse, Http404
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
